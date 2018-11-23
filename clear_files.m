3992d3a1eb07c50c3fc19f0575b08ce01ce2c445
Felipe Fredes <felipe.fredes@ug.uchile.cl>
2018-11-23 14:54:29 -0300

    generalizing code

--- clear_files.m
+++ clear_files.m
@@ -1,5 +1,13 @@
+% Params.txt
 fileID = fopen('C:\\Users\\Felipe Fredes\\Documents\\opnet_res\\params.txt', 'w');
-%formatSpec = '%f'; %  instancia, cwnd, time
-%P = textscan(fileID,formatSpec,'Delimiter','\n');
 fclose(fileID);
-disp('Files have been cleared')
+
+% Fsize files
+fileID = fopen('C:\\Users\\Felipe Fredes\\Documents\\opnet_res\\fsize_c1.txt', 'w');
+fclose(fileID);
+fileID = fopen('C:\\Users\\Felipe Fredes\\Documents\\opnet_res\\fsize_c2.txt', 'w');
+fclose(fileID);
+
+% 
+
+disp('Files have been cleared/initialized')
--- parsing_test.m
+++ parsing_test.m
@@ -40,12 +40,50 @@
 % end
 % fclose(fileID);
 
-fileID = fopen('C:\Felipe\parse_test.txt');
-formatSpec = '%s'; %  instancia, cwnd, time
-P = textscan(fileID,formatSpec);
-text = fileread('C:\Felipe\parse_test.txt');
+% fileID = fopen('C:\Felipe\parse_test.txt');
+% formatSpec = '%s'; %  instancia, cwnd, time
+% P = textscan(fileID,formatSpec);
+% text = fileread('C:\Felipe\parse_test.txt');
+% S = strsplit(text, 'newdata\r\n')
+% format = '%f %f';
+% d1 = textscan(S{2},format,'Delimiter','\r\n');
+% d2 = textscan(S{3},format,'Delimiter','\r\n');
+% fclose(fileID);
+
+
+text = fileread('C:\\Users\\Felipe Fredes\\Documents\\opnet_res\\params.txt');
+delim = 'newdata\r\n';
 S = strsplit(text, 'newdata\r\n')
-format = '%f %f';
-d1 = textscan(S{2},format,'Delimiter','\r\n');
-d2 = textscan(S{3},format,'Delimiter','\r\n');
-fclose(fileID);
+n_sim = length(strfind(text,delim));
+lines = strsplit(S{2}, '\r\n');
+for i = 1:length(lines)
+    tline = lines{i};
+    line_no = i;
+    switch line_no
+        case 1
+            formatSpec = '%f';
+            line_data = textscan(tline,formatSpec);
+            alg_no = line_data{1};
+            switch alg_no
+                case 1
+                    alg_name = 'DropTail';
+                case 2
+                    alg_name = 'RED';
+                case 3
+                    alg_name = 'TTF';
+                otherwise
+                    disp('wrong alg_no')
+            end
+        case 2
+            formatSpec = '%f %f';
+            line_data = textscan(tline,formatSpec);
+            rtt1 = line_data{1};
+            rtt2 = line_data{2};
+        case 3
+            disp('3 is not coded yet!')
+        otherwise
+            disp('no use for this line')
+    end
+end
+
+
