ffdb53e9ff69df7b92d8d3b800cdf9e73b92c4fc
Felipe Fredes <felipe.fredes@ug.uchile.cl>
2018-11-28 01:34:17 -0300

    progress in processing file

--- data_processing.m
+++ data_processing.m
@@ -8,7 +8,7 @@ clc
 %% Parameters
 
 % Choose dataset manually
-datasetStr = '27-Nov-2018_20-28-37';
+datasetStr = '28-Nov-2018_01-23-45';
 results_path = ['./resultados/' datasetStr '/'];
 
 % Change directory to dataset path
@@ -23,8 +23,9 @@ n_sim = size(datasetFiles,1) - 2; % 2 "files" would be "." and ".." in dir's out
 
 % Other parameters
 num_alg = 3; % DropTail, RED and TTF
-num_rtt = 2; % 0.15 and 0.2 for connection 2
-num_bg = 2;  % constant with values: 0.00002 and 0.00001168
+%num_rtt = 2; % 0.15 and 0.2 for connection 2
+%num_bg = 2;  % constant with values: 0.00002 and 0.00001168
+n_seeds = 5;
  
 
 %% Iterate over simulations and read structures
@@ -44,10 +45,36 @@ for i = 1:n_sim
    results_cell{i} = results_struct;
 end
 
-%% Sort structures according to parameters?
+%% Get relevant data
 
+for j=1:n_sims/n_seeds
+    %% Get avg and std for throughput, goodput and effective_throughput
+    
+    %% Get avg and std for (avg) queue size and queueing delay(the previous "amplified" by a factor)
+    
+    
+    dt_qs = {dt_qs_avg, dt_qs_std};
+    red_qs = {red_qs_avg, red_qs_std};
+    ttf_qs = {ttf_qs_avg, ttf_qs_std};
+    %% Get avg and std for RTT (opnet and ttf estimation)
+    
+    
+end
 
 %% Plot data
+
+% Plot "th1" vs "th2" (with "th" = th, gp or eff_th)
+figure()
+
+% Plot queueing_delay / (avg) queue size for every algorithm
+figure()
+errorbar()
+
+% Plot RTT for every algorithm 
+figure()
+errorbar()
+
+% Plot cwnd (must be tested to achieve a meaningful figure)
 
 
 
