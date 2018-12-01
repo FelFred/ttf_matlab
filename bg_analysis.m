3ed8722c227113a0bb0693f2948de57a9d14f4d3
Felipe Fredes <felipe.fredes@ug.uchile.cl>
2018-12-01 18:20:11 -0300

    fixed new file to execute properly

--- bg_analysis.m
+++ bg_analysis.m
@@ -23,10 +23,10 @@ datasetFiles = dir('.');
 n_sim = size(datasetFiles,1) - 2; % 2 "files" would be "." and ".." in dir's output
 
 % Other parameters
-num_alg = 1; % DropTail, RED and TTF
+%num_alg = 1; % DropTail, RED and TTF
 %num_rtt = 2; % 0.15 and 0.2 for connection 2
 %num_bg = 2;  % constant with values: 0.00002 and 0.00001168
-n_seeds = 9;
+%n_seeds = 9;
  
 
 %% Iterate over simulations and read structures
@@ -47,82 +47,54 @@ for i = 1:n_sim
 end
 
 %% Identify algorithms data 
-droptail_cell = cell(n_seeds,1);
-red_cell = cell(n_seeds,1);
-ttf_cell = cell(n_seeds,1);
-droptail_idx = 1;
-red_idx = 1;
-ttf_idx = 1;
 
-% Matrix to store th, gp and eff_th
-droptail_th_array = zeros(3, 2, n_seeds);
-red_th_array = zeros(3, 2, n_seeds);
-ttf_th_array = zeros(3, 2, n_seeds);
+th_array = zeros(3,2,n_sim); % 3 metrics: gp,th,eff_th + 2 connections + 9 simulations, each with a different bg distribution
+q_cell = cell(n_sim,1);
+alg_idx = 1;
 
-% Cells with queue size and RTT estimations
-dt_q_cell = cell(n_seeds,1);
-red_q_cell = cell(n_seeds,1);
-ttf_q_cell = cell(n_seeds,1);
+q_array = zeros(n_sim,1);
+qstd_array = zeros(n_sim,1);
+bg_array = zeros(n_sim,1);
 
-for j=1:n_sim
-   alg = results_cell{j}.alg{1};    
-    
-   if (strcmp(alg,'DropTail'))
-       droptail_cell{droptail_idx} = results_cell{j} ;      
-       droptail_th_array(1, :, droptail_idx) = [results_cell{j}.throughput{1} results_cell{j}.throughput{2}];
-       droptail_th_array(2, :, droptail_idx) = [results_cell{j}.goodput{1} results_cell{j}.goodput{2}];
-       droptail_th_array(3, :, droptail_idx) = [results_cell{j}.th_eff{1} results_cell{j}.th_eff{2}];
-       dt_q_cell{droptail_idx} = results_cell{j}.qstats{1}{2}; % 2 is cur_qsize = instantaneous or smoothed queue size according to smoothing flag value
-       droptail_idx = droptail_idx + 1;
-   elseif (strcmp(alg,'RED'))
-       red_cell{red_idx} = results_cell{j};
-       red_th_array(1, :, red_idx) = [results_cell{j}.throughput{1} results_cell{j}.throughput{2}];
-       red_th_array(2, :, red_idx) = [results_cell{j}.goodput{1} results_cell{j}.goodput{2}];
-       red_th_array(3, :, red_idx) = [results_cell{j}.th_eff{1} results_cell{j}.th_eff{2}]; 
-       red_q_cell{red_idx} = results_cell{j}.qstats{1}{2};
-       red_idx = red_idx + 1;
-   elseif (strcmp(alg,'TTF'))
-       ttf_cell{ttf_idx} = results_cell{j};
-       ttf_th_array(1, :, ttf_idx) = [results_cell{j}.throughput{1} results_cell{j}.throughput{2}];
-       ttf_th_array(2, :, ttf_idx) = [results_cell{j}.goodput{1} results_cell{j}.goodput{2}];
-       ttf_th_array(3, :, ttf_idx) = [results_cell{j}.th_eff{1} results_cell{j}.th_eff{2}]; 
-       ttf_q_cell{ttf_idx} = results_cell{j}.qstats{1}{2};
-       ttf_idx = ttf_idx + 1;       
-   end
+for j=1:n_sim         
+   th_array(1, :, alg_idx) = [results_cell{j}.throughput{1} results_cell{j}.throughput{2}];
+   th_array(2, :, alg_idx) = [results_cell{j}.goodput{1} results_cell{j}.goodput{2}];
+   th_array(3, :, alg_idx) = [results_cell{j}.th_eff{1} results_cell{j}.th_eff{2}];
+   q_cell{alg_idx} = results_cell{j}.qstats{1}{2}; % 2 is cur_qsize = instantaneous or smoothed queue size according to smoothing flag value
+   alg_idx = alg_idx + 1;   
    
+   % Get q data in different arrays for errorbar   (both from instantaneous measurements) 
+   q_array(j) = mean(results_cell{j}.qstats{1}{1});
+   qstd_array(j) = std(results_cell{j}.qstats{1}{1});
+   
+   % Get bg dist array
+   bg_char = char(results_cell{j}.bg_dist);
+   bg_cut = bg_char(11:end-1);
+   formatSpec = '%f';
+   pkt_iat_cell = textscan(bg_cut, formatSpec);
+   pkt_iat = pkt_iat_cell{1};
+   bg_array(j) = pkt_iat;
 end
 
 %% Get avg and std for throughput, goodput and effective_throughput
 % Transform Inf to NaNs (Inf = connection failed)
-droptail_th_array(droptail_th_array == Inf) = nan;
-red_th_array(red_th_array == Inf) = nan;
-ttf_th_array(ttf_th_array == Inf) = nan;
+th_array(th_array == Inf) = nan;
 
-% Get matrix average over seed dimension (ignoring NaNs)
-dt_th = mean(droptail_th_array, 3, 'omitnan');
-red_th = mean(red_th_array, 3, 'omitnan');
-ttf_th = mean(ttf_th_array, 3, 'omitnan');    
+% Get matrix average over bg traffic dimension (ignoring NaNs)
+th = mean(th_array, 3, 'omitnan');   
     
 %% Get avg and std for (avg) queue size and queueing delay(the previous "amplified" by a factor)
-dt_q_array = zeros(n_seeds,2);
-red_q_array = zeros(n_seeds,2);
-ttf_q_array = zeros(n_seeds,2);
+alg_q_array = zeros(n_sim,2);
 
-for k = 1:n_seeds
-    dt_q_array(k,:) = [mean(dt_q_cell{k}) std(dt_q_cell{k})];    
-    red_q_array(k,:) = [mean(red_q_cell{k}) std(red_q_cell{k})];    
-    ttf_q_array(k,:) = [mean(ttf_q_cell{k}) std(ttf_q_cell{k})];
+for k = 1:n_sim
+    alg_q_array(k,:) = [mean(q_cell{k}) std(q_cell{k})];
 end
 
-dt_qs = mean(dt_q_array,1);
-red_qs = mean(red_q_array,1);
-ttf_qs = mean(ttf_q_array,1);
-
-%% Get avg and std for RTT (opnet and ttf estimation)
+alg_qs = mean(alg_q_array,1);
 
 %% Get timeout data arrays
-c1_timeouts = zeros(n_seeds,1);
-c2_timeouts = zeros(n_seeds,1);
+c1_timeouts = zeros(n_sim,1);
+c2_timeouts = zeros(n_sim,1);
 for l = 1:n_sim
     c1_timeouts(l) = length(results_cell{l}.timeouts{1}{1}) - 1;    
     c2_timeouts(l) = length(results_cell{l}.timeouts{2}{1}) - 1;
@@ -131,44 +103,27 @@ end
 %% Plot data
 
 % Plot "th1" vs "th2" (with "th" = th, gp or eff_th)
-gp_array = [dt_th(3,:); red_th(3,:); ttf_th(3,:)];
-algorithms = categorical({'DropTail','RED','TTF'});
+gp_array = squeeze(th_array(3,:,:))';
 figure()
-title('Goodput(c1,c2) for DT, RED, TTF')
-bar(algorithms, gp_array)
-xlabel('Algorithm')
+title('Goodput(c1,c2) for different simulations')
+bar(bg_array, gp_array)
+xlabel('Simulation')
 ylabel('Goodput[bits]')
 
 figure()
-plot(dt_th(3,2), dt_th(3,1), 'ro', 'MarkerSize', 10)
+hold on
+x_plot = squeeze(th_array(3,2,:));
+y_plot = squeeze(th_array(3,1,:));
+plot(x_plot, y_plot, 'r-o', 'MarkerSize', 10)
 xlim([10^5 10^7])
 ylim([10^5 10^7])
-hold on
-plot(red_th(3,2), red_th(3,1), 'ko', 'MarkerSize', 10)
-title('Goodput per connection')
-plot(ttf_th(3,2), ttf_th(3,1), 'bo', 'MarkerSize', 10)
+title('Goodput per connection (fairness)')
 plot([10^5 10^7], [10^5 10^7], 'm--')
-legend('Droptail','RED', 'TTF')
+legend('Algorithm')
 hold off
-
-% Plot queueing_delay / (avg) queue size for every algorithm
-figure()
-errorbar(1,dt_qs(1), dt_qs(2), 'rx')
-title('Queue size average and std through simulation')
-ylim([0 100])
-hold on
-errorbar(2,red_qs(1), red_qs(2), 'kx')
-hold on
-errorbar(3,ttf_qs(1), ttf_qs(2), 'bx')
-legend('Droptail','RED', 'TTF')
-hold off
-
-% Plot RTT for every algorithm 
-% figure()
-% errorbar()
 
 % Plot cwnd (must be tested to achieve a meaningful figure)
-sim_number = 2;
+sim_number = 6;
 c1_cwnd = results_cell{sim_number}.cwnd{1};
 c2_cwnd = results_cell{sim_number}.cwnd{2};
 figure()
@@ -178,34 +133,19 @@ subplot(2,1,2)
 plot(c2_cwnd{2}, c2_cwnd{1})
 
 % Plot timeouts per connection vs simulation
-% sim_array = 1:15;
-% timeouts_array = horzcat(c1_timeouts, c2_timeouts);
-% figure()
-% bar(sim_array, timeouts_array)
-% title('Timeouts per connection vs simulation number')
-% xlabel('Simulation Number')
-% ylabel('Timeouts')
+sim_array = 1:n_sim;
+timeouts_array = horzcat(c1_timeouts, c2_timeouts);
+figure()
+bar(sim_array, timeouts_array)
+title('Timeouts per connection vs simulation number')
+xlabel('Simulation Number')
+ylabel('Timeouts')
 
 %% Plot avg queue size vs bg traffic (RED_TTF)
 
-q_array = zeros(n_sim,1);
-qstd_array = zeros(n_sim,1);
-bg_array = zeros(n_sim,1);
-
-for l = 1:n_sim
-    q_array(l) = mean(results_cell{l}.qstats{1}{1});
-    qstd_array(l) = std(results_cell{l}.qstats{1}{1});
-    bg_char = char(results_cell{l}.bg_dist);
-    bg_cut = bg_char(11:end-1);
-    formatSpec = '%f';
-    pkt_iat_cell = textscan(bg_cut, formatSpec);
-    pkt_iat = pkt_iat_cell{1};
-    bg_array(l) = pkt_iat;
-end
-
-figure()
-plot(bg_array, q_array)
-title('Average queue vs pkt_iat');
+% figure()
+% plot(bg_array, q_array)
+% title('Average queue vs pkt_iat');
 
 figure()
 errorbar(bg_array, q_array, qstd_array)
@@ -222,6 +162,10 @@ subplot(2,1,1)
 plot(results_cell{1}.qstats{1}{4}, results_cell{1}.qstats{1}{1})
 subplot(2,1,2)
 plot(results_cell{1}.qstats{1}{4}, results_cell{1}.qstats{1}{2})
+
+%% Plot loss pdf 
+
+
 %% Return to previous folder
 
 cd ..
