f0c3d80d5af5d1c12de5b1c00b84c559ecfccc85
Felipe Fredes <felipe.fredes@ug.uchile.cl>
2019-01-30 22:21:32 -0300

    added user path as a variable

--- clear_files.m
+++ clear_files.m
@@ -1,73 +1,80 @@
+%% Specify user path
+user_path = 'C:\\Users\\Felipe Fredes\\Documents\\opnet_res\\';
+
+% FF note - 'C:\\Users\\Felipe Fredes\\Documents\\opnet_res\\'
+
+%% Clear/create files
+
 % Params.txt
-fileID = fopen('C:\\Users\\Felipe Fredes\\Documents\\opnet_res\\params.txt', 'w');
+fileID = fopen(strcat(user_path, 'params.txt'), 'w');
 fclose(fileID);
 
 % Fsize files
-fileID = fopen('C:\\Users\\Felipe Fredes\\Documents\\opnet_res\\fsize_c1.txt', 'w');
+fileID = fopen(strcat(user_path, 'fsize_c1.txt'), 'w');
 fclose(fileID);
-fileID = fopen('C:\\Users\\Felipe Fredes\\Documents\\opnet_res\\fsize_c2.txt', 'w');
+fileID = fopen(strcat(user_path, 'fsize_c2.txt'), 'w');
 fclose(fileID);
 
 % Cwnd statistics files
-fileID = fopen('C:\\Users\\Felipe Fredes\\Documents\\opnet_res\\cwndstat_c1.txt', 'w');
+fileID = fopen(strcat(user_path, 'cwndstat_c1.txt'), 'w');
 fclose(fileID);
-fileID = fopen('C:\\Users\\Felipe Fredes\\Documents\\opnet_res\\cwndstat_c2.txt', 'w');
+fileID = fopen(strcat(user_path, 'cwndstat_c2.txt'), 'w');
 fclose(fileID);
 
 % Conn duration files
-fileID = fopen('C:\\Users\\Felipe Fredes\\Documents\\opnet_res\\begin_end_c1.txt', 'w');
+fileID = fopen(strcat(user_path, 'begin_end_c1.txt'), 'w');
 fclose(fileID);
-fileID = fopen('C:\\Users\\Felipe Fredes\\Documents\\opnet_res\\begin_end_c2.txt', 'w');
+fileID = fopen(strcat(user_path, 'begin_end_c2.txt'), 'w');
 fclose(fileID);
 
 % Loss data and total packets files
-fileID = fopen('C:\\Users\\Felipe Fredes\\Documents\\opnet_res\\loss_data.txt', 'w');
+fileID = fopen(strcat(user_path, 'loss_data.txt'), 'w');
 fclose(fileID);
-fileID = fopen('C:\\Users\\Felipe Fredes\\Documents\\opnet_res\\total.txt', 'w');
+fileID = fopen(strcat(user_path, 'total.txt'), 'w');
 fclose(fileID);
 
 % RTT estimations files
-fileID = fopen('C:\\Users\\Felipe Fredes\\Documents\\opnet_res\\oprtt_c1.txt', 'w');
+fileID = fopen(strcat(user_path, 'oprtt_c1.txt'), 'w');
 fclose(fileID);
-fileID = fopen('C:\\Users\\Felipe Fredes\\Documents\\opnet_res\\oprtt_c2.txt', 'w');
-fclose(fileID);
-
-fileID = fopen('C:\\Users\\Felipe Fredes\\Documents\\opnet_res\\est0.txt', 'w');
-fclose(fileID);
-fileID = fopen('C:\\Users\\Felipe Fredes\\Documents\\opnet_res\\est.txt', 'w');
+fileID = fopen(strcat(user_path, 'oprtt_c2.txt'), 'w');
 fclose(fileID);
 
-fileID = fopen('C:\\Users\\Felipe Fredes\\Documents\\opnet_res\\est0_v2.txt', 'w');
+fileID = fopen(strcat(user_path, 'est0.txt'), 'w');
 fclose(fileID);
-fileID = fopen('C:\\Users\\Felipe Fredes\\Documents\\opnet_res\\est_v2.txt', 'w');
+fileID = fopen(strcat(user_path, 'est.txt'), 'w');
+fclose(fileID);
+
+fileID = fopen(strcat(user_path, 'est0_v2.txt'), 'w');
+fclose(fileID);
+fileID = fopen(strcat(user_path, 'est_v2.txt'), 'w');
 fclose(fileID);
 
 % Queue stats file
-fileID = fopen('C:\\Users\\Felipe Fredes\\Documents\\opnet_res\\qstats.txt', 'w');
+fileID = fopen(strcat(user_path, 'qstats.txt'), 'w');
 fclose(fileID);
 
 % Remaining debt file
-fileID = fopen('C:\\Users\\Felipe Fredes\\Documents\\opnet_res\\remdebt.txt', 'w');
+fileID = fopen(strcat(user_path, 'remdebt.txt'), 'w');
 fclose(fileID);
 
 % Timeouts per connection files
-fileID = fopen('C:\\Users\\Felipe Fredes\\Documents\\opnet_res\\timeouts_c1.txt', 'w');
+fileID = fopen(strcat(user_path, 'timeouts_c1.txt'), 'w');
 fclose(fileID);
-fileID = fopen('C:\\Users\\Felipe Fredes\\Documents\\opnet_res\\timeouts_c2.txt', 'w');
+fileID = fopen(strcat(user_path, 'timeouts_c2.txt'), 'w');
 fclose(fileID);
 
 % Loss pdf
-fileID = fopen('C:\\Users\\Felipe Fredes\\Documents\\opnet_res\\loss_pdf.txt', 'w');
+fileID = fopen(strcat(user_path, 'loss_pdf.txt'), 'w');
 fclose(fileID);
 %Prints?
 disp('Files have been cleared/initialized')
 
 % bg end
-fileID = fopen('C:\\Users\\Felipe Fredes\\Documents\\opnet_res\\bg_end.txt', 'w');
+fileID = fopen(strcat(user_path, 'bg_end.txt'), 'w');
 fclose(fileID);
 
 % Pkt destroyed at q files
-fileID = fopen('C:\\Users\\Felipe Fredes\\Documents\\opnet_res\\destroyed.txt', 'w');
+fileID = fopen(strcat(user_path, 'destroyed.txt'), 'w');
 fclose(fileID);
-fileID = fopen('C:\\Users\\Felipe Fredes\\Documents\\opnet_res\\destroyed0.txt', 'w');
+fileID = fopen(strcat(user_path, 'destroyed0.txt'), 'w');
 fclose(fileID);
