e97e64c1cb91695c0612d44360afef854076db0a
Felipe Fredes <felipe.fredes@ug.uchile.cl>
2019-02-01 14:48:58 -0300

    start time of connection obtained from fsize

--- post_opnet.m
+++ post_opnet.m
@@ -191,8 +191,8 @@ for i = 1:n_sim
 %     
 %     dt_c1 = D_0{1}(end) - D_0{1}(end-1);
 %     dt_c2 = D{1}(end) - D{1}(end-1);
-    dt_c1 = C_0{2}(end) - 200;
-    dt_c2 = C{2}(end) - 200;
+    dt_c1 = C_0{2}(end) - (f_size/8)/10^6;
+    dt_c2 = C{2}(end) - (f_size/8)/10^6;
     %% Throughput, goodput and eff throughput
     
     % Goodput per connection
