b0f51ab6ae2f85c279030f75051d4b1eaa13e4b8
F_fredes <felipe.fredes@ug.uchile.cl>
2019-03-15 22:03:17 -0300

    utilization percentage vs time

--- link_utilization.m
+++ link_utilization.m
@@ -6,34 +6,69 @@ fig_number = 1;
 init_time = 0;
 end_time = 600;
 link_rate = 1e8;
+bg_pkt_size = 11744;
+c_pkt_size = 12064;
 window_size = 10;
 full_util = link_rate*window_size;
 dt = 1;
 time_array = init_time:dt:end_time;
 percentage = zeros(length(time_array),1);
 
+
 %% Read data
 output_data = fileread(file_path);
 formatSpec = '%f %f %f';
 line_data = textscan(output_data,formatSpec);
+data1 = line_data{1};
+data2 = line_data{2};
+data3 = line_data{3};
+data_array = horzcat(data1,data2,data3);
+c1_counter = 0;
+c2_counter = 0;
+bg_counter = 0;
+
 
 %% Get info from data
-for i = 1:length(time_array)    
+for i = 1:length(time_array)
+    t_now = time_array(i);
+    ti = t_now - window_size/2;
+    tf = t_now + window_size/2;
+    c1_counter = 0;
+    c2_counter = 0;
+    bg_counter = 0;
+    
+    
     % Get amount of data in current window
-    
-    
+    for j = 1:length(data_array(:,1))        
+        t_j = data_array(j,2);
+        out = data_array(j,1);
+       
+        if ( (t_j<tf) && (t_j >ti) )
+            if (out == 0)
+                c1_counter = c1_counter + 1;
+            elseif (out == 2)
+                c2_counter = c2_counter + 1;
+            elseif (out == 4)
+                bg_counter = bg_counter + 1;
+            end            
+        end                
+    end
+%     bg_counter
+%     c1_counter
+%     c2_counter
+    window_data = bg_counter * bg_pkt_size + (c1_counter+c2_counter) * c_pkt_size;
     
     % Calculate utilization dividing by full utilization
-%     percentage(i) = window_data / full_util;
+    percentage(i) = window_data / full_util;
 end
 
 %% Plot results
-% figure(fig_number)
-% fig_number = fig_number + 1;
-% bar(time_array, percentage')
-% title('Link utilization vs time')
-% xlabel('Time [s]')
-% ylabel('Utilization [%]')
+figure(fig_number)
+fig_number = fig_number + 1;
+bar(time_array, percentage')
+title('Link utilization vs time')
+xlabel('Time [s]')
+ylabel('Utilization [%]')
 
 
 
